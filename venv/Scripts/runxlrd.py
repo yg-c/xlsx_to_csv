b4e837d621f73271c7a79b56e6ecbc9f888cbf7a
yann-w <yancla@mycma.ch>
2020-11-23 13:35:46 +0100

    update gitignore

--- venv/Scripts/runxlrd.py
+++ venv/Scripts/runxlrd.py
@@ -1,4 +1,4 @@
-#!C:\Users\yannc\PycharmProjects\xlsxTOcsv\venv\Scripts\python.exe
+#!C:\Users\yancla\Documents\GitHub\xlsx_to_csv\venv\Scripts\python.exe
 # Copyright (c) 2005-2012 Stephen John Machin, Lingfo Pty Ltd
 # This script is part of the xlrd package, which is released under a
 # BSD-style licence.
